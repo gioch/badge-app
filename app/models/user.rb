c34d9184390052aa00d2f613a9a3ea0366879951
gioch <georgechkhvirkia@gmail.com>
2015-03-18 01:17:43 +0300

    Added helper function to Users model to show user's full name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
     devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
 
+    def full_name
+        self.name + ' ' + self.second_name
+    end
 end
