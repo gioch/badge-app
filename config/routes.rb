88b1cb12b9d37935046dc4289607c668055c1fff
gioch <georgechkhvirkia@gmail.com>
2015-03-18 01:16:26 +0300

    Seeding admin user and basic user with seed file

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  root 'zones#index'
+
   scope 'admin' do
     resources 'companies'
     resources 'users'
@@ -10,6 +12,6 @@ Rails.application.routes.draw do
 
   get '/dashboard', to: 'dashboard#index', as: 'dashboard'
 
-  root 'zones#index'
+  
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin_user = User.new
+admin_user.email = 'admin@mail.com'
+admin_user.password = 'qwerasdf'
+admin_user.name = 'George'
+admin_user.second_name = 'Chkhvirkia'
+admin_user.admin = 1
+admin_user.save
+
+user = User.new
+user.email = 'user@mail.com'
+user.password = 'qwerasdf'
+user.name = 'Steve'
+user.second_name = 'Jobs'
+user.save
