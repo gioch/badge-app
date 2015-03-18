72f6f27cbf069760867ef6fcb9faef6e78387e56
gioch <georgechkhvirkia@gmail.com>
2015-03-19 02:01:36 +0300

    Added zone update functionality

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -21,9 +21,24 @@ class ZonesController < ApplicationController
     end
 
     def edit
+        @zone = find_zone
+    end
+
+    def update
+        @zone = find_zone
+
+        if @zone.update(zone_params)
+            redirect_to zones_path
+        else
+            render 'edit'
+        end
     end
 
     private
+        def find_zone
+            Zone.find(params[:id])
+        end
+
         def zone_params
             params.require(:zone).permit(:name, :color, :all_access)
         end
