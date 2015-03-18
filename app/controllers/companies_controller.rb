d954d0daf7539eb7bad12f3ab6b342269c389e70
gioch <georgechkhvirkia@gmail.com>
2015-03-18 22:33:32 +0300

    Added basic company create functionality

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,10 +3,25 @@ class CompaniesController < ApplicationController
     before_filter :admin_authorized?
 
     def index
+        @companies = Company.all
     end
 
     def new
+        @company = Company.new
     end
 
+    def create
+        @company = Company.create(company_params)
 
+        if @company.save
+            redirect_to companies_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+        def company_params
+            params.require(:company).permit(:name);
+        end
 end
