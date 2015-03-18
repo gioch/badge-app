9685fba86f2872e6ffc68cd6f305409ccfc6fe8c
gioch <georgechkhvirkia@gmail.com>
2015-03-18 18:38:02 +0300

    Creating User forms with form helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
     
     def index
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        redirect_to users_path
+    end
 end
