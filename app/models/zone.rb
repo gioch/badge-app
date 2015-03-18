8e6589f2495713e0f8fc0aac4854bfdbd5cbc765
gioch <georgechkhvirkia@gmail.com>
2015-03-18 22:58:49 +0300

    added regular expression to color validator

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,4 +1,4 @@
 class Zone < ActiveRecord::Base
     validates :name, presence: :true
-    validates :color, presence: :true
+    validates_format_of :color, presence: :true, with: /\A^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$\z/
 end
