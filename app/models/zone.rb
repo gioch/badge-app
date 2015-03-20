d2101e7e065551a933dfb85aa0edca6c0537285f
gioch <georgechkhvirkia@gmail.com>
2015-03-20 13:24:12 +0300

    Fixed typo in comment

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -3,7 +3,7 @@ class Zone < ActiveRecord::Base
 
     validates :name, presence: :true
     
-    # Valdiates color's HEX format
+    # Validates color's HEX format
     validates_format_of :color, presence: :true, with: /\A^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$\z/
 
 end
